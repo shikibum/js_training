70ac371ff0581c51241823d2a6c94cc2637319d7
Rina Mito <satoshikibu@gmail.com>
2018-04-20 17:15:59 +0900

    第9回

--- fortune/main.js
+++ fortune/main.js
@@ -4,10 +4,19 @@
   var btn = document.getElementById('btn');
 
   btn.addEventListener('click', function() {
-    var results = ['大吉', '中吉', '末吉', '凶'];
-    // var n =  Math.floor(Math.random() * 4);
-    var n =  Math.floor(Math.random() * results.length);
-    this.textContent = results[n];
+    var n = Math.random();
+    if (n < 0.05) {
+      this.textContent = '大吉';
+    } else if (n < 0.2) {
+      this.textContent = '中吉';
+    } else {
+      this.textContent = ' 凶';
+    }
+    // // var results = ['大吉', '中吉', '末吉', '凶'];
+    // var results = ['大吉', '大吉', '大吉', '大吉', '大吉', '中吉', '末吉', '凶'];
+    // // var n =  Math.floor(Math.random() * 4);
+    // var n =  Math.floor(Math.random() * results.length);
+    // this.textContent = results[n];
   });
 
   btn.addEventListener('mousedown', function() {
