80171fbc85bc4bc22a5f732d856f5abf6ffe7fb9
Rina Mito <satoshikibu@gmail.com>
2018-04-20 17:02:35 +0900

    第7回まで

--- fortune/main.js
+++ fortune/main.js
@@ -5,7 +5,25 @@
 
   btn.addEventListener('click', function() {
     var n =  Math.floor(Math.random() * 3);
-    this.textContent = n;
+    // if (n === 0) {
+    //   this.textContent = '大吉';
+    // } else if (n === 1) {
+    //   this.textContent = '吉';
+    // }
+    // else if (n === 2) {
+    //   this.textContent = '凶';
+    // }
+    switch (n) {
+      case 0:
+        this.textContent = '大吉';
+        break;
+      case 1:
+        this.textContent = '吉';
+        break;
+      case 2:
+        this.textContent = '凶';
+        break;
+    }
   });
 
   btn.addEventListener('mousedown', function() {
