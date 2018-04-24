56343d67e9fe9fd0fbebdc18708d1313b5d8256b
Rina Mito <satoshikibu@gmail.com>
2018-04-24 15:58:49 +0900

    第7回

--- tabmenu/main_tabmenu.js
+++ tabmenu/main_tabmenu.js
@@ -2,6 +2,7 @@
   'use strict';
 
   var menuItems = document.getElementsByClassName('menu_item');
+  var contents = document.getElementsByClassName('content');
 
   var i;
 
@@ -15,6 +16,12 @@
         menuItems[i].className = 'menu_item';
       }
       this.className = 'menu_item active';
+
+      for (i = 0; i < contents.length; i++) {
+        contents[i].className = 'content';
+      }
+
+      document.getElementById(this.dataset.id).className = 'content active';
 
     });
   }
